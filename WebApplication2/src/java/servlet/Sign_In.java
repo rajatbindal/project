43fdc16c284e52ce78a5afd6a6b4651bdec447c3
rajatbindal <43265175+rajatbindal@users.noreply.github.com>
2018-12-03 15:39:00 +0530

    Add files via upload

---
+++ WebApplication2/src/java/servlet/Sign_In.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.*;
+
+import java.sql.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Rajat Bindal
+ */
+
+public class Sign_In extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            Class.forName("com.mysql.cj.jdbc.Driver");   
+           Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/mydb?useSSL=false","root","rajatbindal");   
+           
+           Statement stmt = conn.createStatement();
+           ResultSet rs = stmt.executeQuery("SELECT* FROM signup_table");
+        String s1 = request.getParameter("mail");
+        String s2 = request.getParameter("pass");
+       int flag=0;
+           while(rs.next())
+           {
+               if(s1.equals(rs.getString("email")))
+               {
+                   if(s2.equals(rs.getString("password")))
+                   {
+                       flag=1;
+                   }
+               }
+           }
+           if(flag==1)
+           {
+               response.sendRedirect("homepage.html");
+           }
+           else
+           {
+               out.println("Login unsuccessful");
+           }
+        }
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
